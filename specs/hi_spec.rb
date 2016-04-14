0d2376d6d3b1426914228dc3135a5e627727fe89
Takafumi Ikeda <ikeike443@github.com>
2016-04-14 09:12:57 +0900

    Update hi_spec.rb

--- specs/hi_spec.rb
+++ specs/hi_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path '../spec_helper.rb', __FILE__
 
 describe "My Sinatra Application" do
   it "should allow accessing /hi" do
-    get '/h'
+    get '/hi'
     expect(last_response).to be_ok
   end
 end
